598ad945c9edcc0f1d0b9945ac5e58b0bc0b9a30
Zulko <valentin.zulkower@gmail.com>
2016-09-06 19:42:46 +0100

    Added example !

--- dna_features_viewer/__init__.py
+++ dna_features_viewer/__init__.py
@@ -1,6 +1,6 @@
 """ dna_features_viewer/__init__.py """
 
- __all__ = ["GraphicRecord", "GraphicFeature"]
+__all__ = ("GraphicRecord", "GraphicFeature")
 
 from .dna_features_viewer import *
 
---
+++ examples/by_hand.py
@@ -0,0 +1,14 @@
+from dna_features_viewer import GraphicFeature, GraphicRecord
+features=[
+    GraphicFeature(start=0, end=20, strand=+1, color="#ffd700",
+                   label="Small feature"),
+    GraphicFeature(start=20, end=500, strand=+1, color="#ffcccc",
+                   label="Gene 1 with a very long name"),
+    GraphicFeature(start=400, end=700, strand=-1, color="#cffccc",
+                   label="Gene 2"),
+    GraphicFeature(start=600, end=900, strand=+1, color="#ccccff",
+                   label="Gene 3")
+]
+record = GraphicRecord(sequence_length=1000, features=features)
+ax, _ = record.plot(fig_width=5)
+ax.figure.savefig("by_hand.png")
