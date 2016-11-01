b720bae18345d7759ceef1e5584046f7ec983eb2
Zulko <valentin.zulkower@gmail.com>
2016-11-01 19:03:43 +0000

    Now with circular graphics !

--- dna_features_viewer/dna_features_viewer.py
+++ dna_features_viewer/dna_features_viewer.py
@@ -72,59 +72,6 @@ class GraphicFeature:
                               strand=feature.location.strand,
                               color=color, label=label)
 
-    def create_patch(self, level):
-        """Create an Arrow Matplotlib patch with the feature's coordinates.
-
-        The Arrow points in the direction of the feature's strand.
-        If the feature has no direction (strand==0), the returned patch will
-        simply be a rectangle.
-
-        The x-coordinates of the patch are determined by the feature's
-        `start` and `end` while the y-coordinates are determined by the `level`
-        """
-        x1, x2 = self.start, self.end
-        if self.strand == -1:
-            x1, x2 = x2, x1
-        head_length = 5 if self.strand in (-1, 1) else 0
-        arrowstyle = mpatches.ArrowStyle.Simple(head_width=14,
-                                                tail_width=14,
-                                                head_length=head_length)
-        return mpatches.FancyArrowPatch([x1, level], [x2, level],
-                                        shrinkA=0.0, shrinkB=0.0,
-                                        arrowstyle=arrowstyle,
-                                        facecolor=self.color)
-
-    def annotate(self, ax, level):
-        """Create a Matplotlib Text with the feature's label.
-
-        The x-coordinates of the text are determined by the feature's
-        `x_center` while the y-coordinates are determined by the `level`.
-
-        The text is horizontally and vertically centered.
-
-        The Arrow points in the direction of the feature's strand.
-        If the feature has no direction (strand==0), the returned patch will
-        simply be a rectangle.
-
-        The x-coordinates of the patch are determined by the feature's
-        `start` and `end` while the y-coordinates are determined by the `level`
-        """
-        bg_color = change_luminosity(self.color, luminosity=0.95)
-        text = ax.text(
-            self.x_center, level, self.label,
-            horizontalalignment="center",
-            verticalalignment="center",
-            bbox=dict(boxstyle="round", fc=bg_color, ec="0.5", lw=1)
-        )
-        margin = 0.1 * abs(self.end - self.start)
-        x1, y1, x2, y2 = get_text_box(text, margin=margin)
-        overflowing = (x1 < self.start) or (x2 > self.end)
-        return text, overflowing, (x1, x2)
-
-    def plot(self, ax, level=0):
-        """Plot the feature's Matplotlib patch on a Matplotlib ax."""
-        ax.add_patch(self.create_patch(level=level))
-
     def __repr__(self):
         return (("GF(%(label)s, %(start)d-%(end)d " % self.__dict__) +
                 (")" if self.strand is None else "(%d))" % self.strand))
@@ -143,48 +90,17 @@ class GraphicRecord:
       list of GraphicalFeature objects.
     """
 
-    def __init__(self, sequence_length, features):
+    def __init__(self, sequence_length, features, feature_level_width=1,
+                 annotation_level_width=1):
         self.features = features
         self.sequence_length = sequence_length
+        self.feature_level_width = feature_level_width
+        self.annotation_level_width = annotation_level_width
 
-    def plot(self, ax=None, fig_width=8, draw_line=True, with_ruler=True):
-        """Plot all the features in the same Matplotlib ax
+    def initialize_ax(self, ax, draw_line, with_ruler):
 
-        `fig_width` represents the width in inches of the final figure (if
-        no `ax` parameter is attributed).
-        """
-        levels = compute_features_levels(self.features)
-        max_level = 1 if (levels == {}) else max(1, max(levels.values()))
-        auto_figure_height = ax is None
-        if ax is None:
-            fig, ax = plt.subplots(1, figsize=(fig_width, 2 * max_level))
-            ax.set_ylim(-1, max_level + 1),
-            ax.set_xlim(0, self.sequence_length)
-        ax.set_xlim(0, self.sequence_length)
-        ax.set_ylim(-1, max_level + 1)
         if draw_line:
             ax.axhline(0, zorder=-1000, c="k")
-        overflowing_annotations = []
-        for feature, level in levels.items():
-            feature.plot(ax=ax, level=level)
-            text, overflowing, (x1, x2) = feature.annotate(ax=ax, level=level)
-            if overflowing:
-                ft = GraphicFeature(start=x1, end=x2, feature=feature,
-                                    text=text, feature_level=level)
-                overflowing_annotations.append(ft)
-        levels = compute_features_levels(overflowing_annotations)
-        max_y = max_level + 1
-        for feature, level in levels.items():
-            text = feature.data["text"]
-            ft = feature.data["feature"]
-            x, y = text.get_position()
-            new_y = max_level + 1 + level
-            max_y = max(max_y, new_y)
-            text.set_position((x, new_y))
-            xx = [x, feature.data["feature"].x_center]
-            yy = [new_y, feature.data["feature_level"]]
-            ax.plot(xx, yy, c="#cccccc", lw=1, zorder=-1000,)
-        ax.set_ylim(-1, max_y + 1)
 
         if with_ruler:  # only display the xaxis ticks
             ax.set_frame_on(False)
@@ -193,12 +109,125 @@ class GraphicRecord:
         else:  # don't display anything
             ax.axis("off")
 
-        if auto_figure_height:
-            ax.figure.set_size_inches(fig_width, 1.5 + 0.37 * max_y)
-        return ax, max_y
+        ax.set_xlim(0, self.sequence_length)
 
-    @staticmethod
-    def from_biopython_record(record, features_filter=None,
+    def plot_feature(self, ax, feature, level):
+        """Create an Arrow Matplotlib patch with the feature's coordinates.
+
+        The Arrow points in the direction of the feature's strand.
+        If the feature has no direction (strand==0), the returned patch will
+        simply be a rectangle.
+
+        The x-coordinates of the patch are determined by the feature's
+        `start` and `end` while the y-coordinates are determined by the `level`
+        """
+        x1, x2 = feature.start, feature.end
+        if feature.strand == -1:
+            x1, x2 = x2, x1
+        head_length = 5 if feature.strand in (-1, 1) else 0
+        arrowstyle = mpatches.ArrowStyle.Simple(head_width=14,
+                                                tail_width=14,
+                                                head_length=head_length)
+        patch = mpatches.FancyArrowPatch([x1, level], [x2, level],
+                                         shrinkA=0.0, shrinkB=0.0,
+                                         arrowstyle=arrowstyle,
+                                         facecolor=feature.color, zorder=0)
+        ax.add_patch(patch)
+        return patch
+
+    def coordinates_in_plot(self, x, level):
+        return (x, level * self.feature_level_width)
+
+    def annotate_feature(self, ax, feature, level, fontsize=11,
+                         box_linewidth=1, box_color=None):
+        """Create a Matplotlib Text with the feature's label.
+
+        The x-coordinates of the text are determined by the feature's
+        `x_center` while the y-coordinates are determined by the `level`.
+
+        The text is horizontally and vertically centered.
+
+        The Arrow points in the direction of the feature's strand.
+        If the feature has no direction (strand==0), the returned patch will
+        simply be a rectangle.
+
+        The x-coordinates of the patch are determined by the feature's
+        `start` and `end` while the y-coordinates are determined by the `level`
+        """
+        bg_color = change_luminosity(feature.color, luminosity=0.95)
+        x, y = self.coordinates_in_plot(feature.x_center, level)
+        text = ax.text(
+            x, y, feature.label,
+            horizontalalignment="center",
+            verticalalignment="center",
+            bbox=dict(boxstyle="round", fc=bg_color if box_color is None
+                      else box_color, ec="0.5",
+                      lw=box_linewidth),
+            fontsize=fontsize,
+            zorder=2
+        )
+        margin = 0.1 * abs(feature.end - feature.start)
+        x1, y1, x2, y2 = get_text_box(text, margin=margin)
+        overflowing = (x1 < feature.start) or (x2 > feature.end)
+        return text, overflowing, (x1, x2)
+
+    def plot(self, ax=None, fig_width=8, draw_line=True, with_ruler=True,
+             fontsize=11, box_linewidth=1, box_color=None,
+             annotate_inline=False):
+        """Plot all the features in the same Matplotlib ax
+
+        `fig_width` represents the width in inches of the final figure (if
+        no `ax` parameter is attributed).
+        """
+        features_levels = compute_features_levels(self.features)
+        max_level = (1 if (features_levels == {}) else
+                     max(1, max(features_levels.values())))
+        auto_figure_height = ax is None
+        if ax is None:
+            fig, ax = plt.subplots(1, figsize=(fig_width, 2 * max_level))
+        self.initialize_ax(ax, draw_line=draw_line, with_ruler=with_ruler)
+        overflowing_annotations = []
+        for feature, level in features_levels.items():
+            self.plot_feature(ax=ax, feature=feature, level=level)
+            if feature.label is not None:
+                text, overflowing, (x1, x2) = self.annotate_feature(
+                    ax=ax, feature=feature, level=level,
+                    box_linewidth=box_linewidth, box_color=box_color
+                )
+                if overflowing or not annotate_inline:
+                    overflowing_annotations.append(GraphicFeature(
+                        start=x1, end=x2, feature=feature,
+                        text=text, feature_level=level
+                    ))
+
+        annotations_levels = compute_features_levels(overflowing_annotations)
+        for feature, level in annotations_levels.items():
+            text = feature.data["text"]
+            x, y = text.get_position()
+            new_y = ((max_level + 1) * self.feature_level_width +
+                     (level) * self.annotation_level_width)
+            text.set_position((x, new_y))
+            fx, fy = self.coordinates_in_plot(feature.data["feature"].x_center,
+                                              feature.data["feature_level"])
+            ax.plot([x, fx], [new_y, fy], c="k", lw=0.5, zorder=1)
+
+        self.finalize_ax(ax, max(features_levels.values()),
+                         max(annotations_levels.values()),
+                         auto_figure_height)
+        return ax
+
+    def finalize_ax(self, ax, features_levels, annotations_levels,
+                    auto_figure_height=False):
+
+        ymax = (self.feature_level_width * (features_levels + 1) +
+                self.annotation_level_width * (annotations_levels + 1))
+        ax.set_ylim(-1, ymax)
+        if auto_figure_height:
+            fig_width = ax.figure.get_size_inches()[0]
+            ax.figure.set_size_inches(fig_width, 1.5 + 0.37 * ymax)
+
+    @classmethod
+    def from_biopython_record(cls, record, features_filter=None,
                               fun_color=None, fun_label=None):
         """Create a new GraphicRecord from a BioPython Record object.
 
@@ -240,7 +269,7 @@ class GraphicRecord:
             if feature.location is not None
             if features_filter(feature)
         ]
-        return GraphicRecord(sequence_length=len(record.seq),
+        return cls(sequence_length=len(record.seq),
                              features=features)
 
     def to_biopython_record(self):
@@ -263,3 +292,138 @@ class GraphicRecord:
         ]
         sequence = Seq(self.data["sequence"], alphabet=DNAAlphabet())
         return SeqRecord(sequence=sequence, features=features)
+
+
+import matplotlib.patches as mpatches
+from dna_features_viewer import GraphicRecord
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class ArrowWedge(mpatches.Wedge):
+
+    def __init__(self, center, radius, theta1, theta2, width, direction=+1,
+                 **kwargs):
+
+        self.direction = direction
+        self.radius = radius
+        mpatches.Wedge.__init__(self, center, radius,
+                                theta1, theta2, width, **kwargs)
+        self._recompute_path()
+
+    def _recompute_path(self):
+
+        if not self.direction in [-1, +1]:
+            return Wedge._recompute_path(self)
+
+        theta1, theta2 = self.theta1, self.theta2
+        arrow_angle = min(5, abs(theta2 - theta1) / 2)
+        normalized_arrow_width = self.width / 2.0 / self.radius
+        if self.direction == +1:
+            angle_start_arrow = theta1 + arrow_angle
+            arc = mpatches.Path.arc(angle_start_arrow, theta2)
+            outer_arc = arc.vertices[::-1] * (1 + normalized_arrow_width)
+            inner_arc = arc.vertices * (1 - normalized_arrow_width)
+            arrow_vertices = [
+                outer_arc[-1],
+                np.array([np.cos(np.deg2rad(theta1)),
+                          np.sin(np.deg2rad(theta1))]),
+                inner_arc[0]
+            ]
+        else:
+            angle_start_arrow = theta2 - arrow_angle
+            arc = mpatches.Path.arc(theta1, angle_start_arrow)
+            outer_arc = arc.vertices * \
+                (self.radius + self.width / 2.0) / self.radius
+            inner_arc = arc.vertices[
+                ::-1] * (self.radius - self.width / 2.0) / self.radius
+            arrow_vertices = [
+                outer_arc[-1],
+                np.array([np.cos(np.deg2rad(theta2)),
+                          np.sin(np.deg2rad(theta2))]),
+                inner_arc[0]
+            ]
+        p = np.vstack([outer_arc, arrow_vertices, inner_arc])
+
+        path_vertices = np.vstack([p, inner_arc[-1, :], (0, 0)])
+
+        path_codes = np.hstack([arc.codes,
+                                4 * [mpatches.Path.LINETO],
+                                arc.codes[1:],
+                                mpatches.Path.LINETO,
+                                mpatches.Path.CLOSEPOLY])
+        path_codes[len(arc.codes)] = mpatches.Path.LINETO
+
+        # Shift and scale the wedge to the final location.
+        path_vertices *= self.r
+        path_vertices += np.asarray(self.center)
+        self._path = mpatches.Path(path_vertices, path_codes)
+
+
+class CircularGraphicRecord(GraphicRecord):
+
+    def __init__(self, sequence_length, features, top_position=0,
+                 feature_level_width=0.2, annotation_level_width=0.2):
+
+        self.radius = 1.0
+        self.sequence_length = sequence_length
+        self.features = features
+        self.top_position = top_position
+        self.angle_rotation = 2 * np.pi * top_position / sequence_length
+        self.feature_level_width = feature_level_width
+        self.annotation_level_width = annotation_level_width
+
+    def initialize_ax(self, ax, draw_line, with_ruler):
+
+        if draw_line:
+            circle = mpatches.Circle(
+                (0, -self.radius), self.radius, facecolor='none')
+            ax.add_patch(circle)
+        ax.axis("off")
+        if with_ruler:  # only display the xaxis ticks
+            ax.set_frame_on(False)
+            ax.yaxis.set_visible(False)
+            ax.xaxis.tick_bottom()
+        else:  # don't display anything
+            ax.axis("off")
+
+        ax.set_xlim(-self.radius,self.radius)
+        ax.set_aspect("equal")
+
+    def finalize_ax(self, ax, features_levels, annotation_levels,
+                    auto_figure_height=False):
+        ymin = (-2 * self.radius -
+                self.feature_level_width * (features_levels + 1))
+        ymax = (self.feature_level_width * (features_levels + 1) +
+                (annotation_levels + 1) * self.annotation_level_width)
+        xmin = -self.radius - self.feature_level_width * (features_levels + 1)
+        xmax = -xmin
+        ax.set_xlim(xmin, xmax)
+        ax.set_ylim(ymin, ymax)
+        ratio = 1.0*(ymax - ymin)/(xmax - xmin)
+
+        if auto_figure_height:
+            fig_width = ax.figure.get_size_inches()[0]
+            ax.figure.set_size_inches(fig_width, fig_width *ratio)
+
+    def plot_feature(self, ax, feature, level):
+        a_start = self.position_to_angle(feature.start)
+        a_end = self.position_to_angle(feature.end)
+        a_start, a_end = sorted([a_start, a_end])
+        r = self.radius + level * self.feature_level_width
+        patch = ArrowWedge((0, -self.radius), r, a_start, a_end,
+                           0.7 * self.feature_level_width,
+                           direction=feature.strand,
+                           facecolor=feature.color, zorder=1)
+        ax.add_patch(patch)
+
+    def position_to_angle(self, position):
+        a = 360.0 * (position - self.top_position) / self.sequence_length
+        return 90 - a
+
+    def coordinates_in_plot(self, x, level):
+        r = self.radius + level * self.feature_level_width
+        angle = self.position_to_angle(x)
+        rad_angle = np.deg2rad(angle)
+        return np.array([r * np.cos(rad_angle),
+                         r * np.sin(rad_angle) - self.radius])
