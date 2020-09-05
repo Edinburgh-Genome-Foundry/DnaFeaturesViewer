a73ef5dc14696abeb06922826ea51f5d71388fde
Peter Vegh <pvegh@posteo.uk>
2020-09-05 14:06:49 +0100

    License autodetect fix

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('dna_features_viewer/version.py').read()) # loads __version__
+exec(open("dna_features_viewer/version.py").read())  # loads __version__
 
-setup(name='dna_features_viewer',
-      version=__version__,
-      author='Zulko',
-    description='Plot features from DNA sequences (e.g. Genbank) with Python',
-    long_description=open('pypi-readme.rst').read(),
-    url='https://github.com/Edinburgh-Genome-Foundry/DnaFeaturesViewer',
-    license='MIT',
+setup(
+    name="dna_features_viewer",
+    version=__version__,
+    author="Zulko",
+    description="Plot features from DNA sequences (e.g. Genbank) with Python",
+    long_description=open("pypi-readme.rst").read(),
+    url="https://github.com/Edinburgh-Genome-Foundry/DnaFeaturesViewer",
+    license="MIT",
     keywords="DNA Sequence Feature Genbank Biopython Matplotlib",
-    packages= find_packages(exclude='docs'),
-    install_requires=["matplotlib>=3", "Biopython"])
+    packages=find_packages(exclude="docs"),
+    install_requires=["matplotlib>=3", "Biopython"],
+)
